aae7630a020825a66d65304212e4921c8f66f0f2
Your Name <you@example.com>
2021-08-07 20:08:24 +0800

    add re

--- jenkinsOps/jenkins_ops.py
+++ jenkinsOps/jenkins_ops.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import re
 import json
+from xml.dom import minidom
+
 import jenkins
 
 class JenkinsOps(object):
@@ -22,19 +25,34 @@ class JenkinsOps(object):
 
     def get_project(self, *, view="ALL"):
         jobs = []
+        urls = []
         if view == "ALL":
             data = json.dumps(self.conn.get_all_jobs(),indent=2, ensure_ascii=False)
             projects = json.loads(data)
             for project in projects:
                 jobs.append(project["fullname"])
-                #print(project["fullname"])
-            print("ALL: ", jobs)
+                urls.append(project['url'])
         else:
             data = json.dumps(self.conn.get_jobs(view_name=view))
             projects = json.loads(data)
             for project in projects:
                 jobs.append(project['name'])
-            print("%s: %s" % (view, jobs))
+                urls.append(project['url'])
+        return jobs
+        #print("%s: %s: %s" % (view, jobs, urls))
+
+    def get_job_config(self, jobName='default'):
+        data = minidom.parseString(self.conn.get_job_config(jobName))
+        config = data.toprettyxml()
+        element = data.documentElement.getElementsByTagName('remoteURL')[0].firstChild.nodeValue
+        print("%s" % element)
+
+    def re_get_job_config(self, jobName='default', regx=''):
+        data = minidom.parseString(self.conn.get_job_config(jobName))
+        config = data.toprettyxml()
+        reg = re.compile(regx)
+        line = reg.search(config).group(1)
+        print("%s" % line)
 
     def create_view(self, name='default'):
         self.conn.create_view(name, jenkins.EMPTY_VIEW_CONFIG_XML)
